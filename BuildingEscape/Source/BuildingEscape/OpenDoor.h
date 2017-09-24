f415e199cc0200c537baec536d0e8442eaa9295e
Allen Kashiwa <358804906@qq.com>
2017-09-24 21:13:43 +0800

    Macros Starting with UPROPERTY

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -24,6 +24,12 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
-	
+private:
+    UPROPERTY(VisibleAnywhere)
+    float OpenAngle = 90.0f;
+
+    UPROPERTY(EditAnywhere)
+    class ATriggerVolume* PressurePlate;
+
+
 };
